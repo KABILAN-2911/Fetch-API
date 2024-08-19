b9b20a506d230861be2bc62e5fbedaebe0986e64
KABILAN-2911 <141658878+KABILAN-2911@users.noreply.github.com>
2024-08-19 09:52:38 +0530

    initial

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState,useEffect } from 'react';
+
+
+
+function Userdemo(){
+  const [users,setUsers] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+useEffect(()=>{
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data =>{
+    setUsers(data);
+    setLoading(false);
+    })
+  .catch (error =>{
+      console.error("if it is not loading will be error message");
+
+      setLoading(false);
+  });
+},[]);
+  if(loading){
+      return <p>Loading..</p>;
+  }
+
+  return(
+      <div>
+      <h1>List out the Users in API</h1>
+      <ul>
+          {users.map(user => (
+              <li key={user.id}>{user.name},{user.email}</li>
+          ))}
+      </ul>
+      </div>
+  );
+}
+ReactDOM.render(<Userdemo />,document.getElementById("root")) 
